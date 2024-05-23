82a9bfc1ffc48532a1b2b9bc1959ef15fee27083
waweruedwin8 <waweruedwin8@gmail.com>
2024-05-23 03:03:16 +0300

    user input, comments, fstrings, variables, data types, print function

---
+++ python_module/user_input.py
@@ -0,0 +1,8 @@
+
+#  a program that asks the user for their name, age, and location and then prints out a personalized message.
+print("Hello there")
+name = input("what is your name?")
+age = int(input("what is your  age?"))
+location = input("where do you live?")
+
+print(f"Hello {name}, you are {age} years old and live in {location} very nice to meet yah!")
